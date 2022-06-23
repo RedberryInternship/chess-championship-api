2ed38a0fbac2fa32cee4cdae12c241e2ebd33d2f
giunashvili <giorgi.official@yahoo.com>
2022-06-23 14:31:22 +0400

    feat: add swagger for api documentation

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,19 @@
 import express from "express"
 import {playersController, registerController} from 'controllers'
+import YAML from 'yamljs'
+import path from "path"
+import swaggerUI, {SwaggerUiOptions} from 'swagger-ui-express'
+
+const swaggerDocument = YAML.load(path.join(__dirname, './config/swagger.yaml'))
+const options: SwaggerUiOptions = {
+    customCss: '.swagger-ui .topbar { display: none }',
+    customSiteTitle: 'Chess Tournament API',
+}
 
 const router = express.Router()
 
 router.get('/grandmasters', playersController.all)
 router.post('/register', registerController.register)
+router.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument, options))
 
 export default router
