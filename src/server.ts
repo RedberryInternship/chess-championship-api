dc422545a7758c158eb2b0eedc87e636cb95f1a6
giunashvili <giorgi.official@yahoo.com>
2022-06-24 15:24:11 +0400

    feat: add cors middlware

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import express from 'express'
 import chalk from "chalk"
+import cors from 'cors'
 import router from 'router'
 import {join} from 'path'
 import {indexController} from 'controllers'
@@ -9,7 +10,7 @@ const server = express()
 
 server.use('/', express.static(join(__dirname, '../public')))
 server.get('/', indexController.index)
-server.use('/api', router)
+server.use('/api', cors() ,router)
 
 server.listen(process.env.APP_PORT, () => console.log(
     chalk.bold.blueBright(`Server started at http://localhost:${process.env.APP_PORT}`)
